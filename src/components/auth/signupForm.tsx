88ee61c35bc545b1d010dbead5ba8c5ddb9bde76
Kishore <kishoresmile08@gmail.com.com>
2025-09-24 21:24:59 +0530

    Form changes

--- src/components/auth/signupForm.tsx
+++ src/components/auth/signupForm.tsx
@@ -213,7 +213,7 @@ export default function SignupForm() {
                 <select
                   id="departmentId"
                   name="departmentId"
-                  required
+                 // required
                   value={formData.departmentId}
                   onChange={(e) => setFormData(prev => ({...prev, departmentId: e.target.value}))}
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
